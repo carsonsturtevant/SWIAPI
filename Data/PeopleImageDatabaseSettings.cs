82216ae00cdbed2f995ae92e3c7a3ccbf8ce24d9
carsonsturtevant <carson.sturtevant@gmail.com>
2020-02-13 23:03:20 -0700

    initial commit

---
+++ Data/PeopleImageDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace SWIAPI.Data
+{
+    public class PeopleImageDatabaseSettings : IPeopleImageDatabaseSettings
+    {
+        public string PeopleImageCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IPeopleImageDatabaseSettings
+    {
+        string PeopleImageCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
---
+++ Models/PeopleImage.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SWIAPI.Models
+{
+    public class PeopleImage
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public int SwapiPeopleId { get; set; }
+        public string ImageUrl { get; set; }
+
+        //[BsonElement("Name")] used to define the column name
+    }
+}
